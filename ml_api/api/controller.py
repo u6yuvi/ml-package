52d545b489ce43465fb1b9e1ca783e1e34a7a350
Utkarsh <u6yuvi@gmail.com>
2019-11-05 23:55:35 +0530

    prediction_endpoint

--- ml_api/api/controller.py
+++ ml_api/api/controller.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, request
-
+from flask import Blueprint, request ,jsonify
+from regression_model.predict import make_prediction
 from api.config import get_logger
 
 _logger = get_logger(logger_name=__name__)
@@ -13,3 +13,16 @@ def health():
     if request.method == 'GET':
         _logger.info('health status OK')
         return 'ok'
+
+
+@prediction_app.route('/v1/predict/regression', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        json_data = request.get_json()
+        _logger.info(f'Inputs: {json_data}')
+        result = make_prediction(input_data=json_data)
+        _logger.info(f'Outputs: {result}')
+        predictions = result.get('predictions')[0]
+        version = result.get('version')
+        return jsonify({'predictions': predictions,
+                         'version': version})
